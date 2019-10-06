6f59ecd3149ae2b755fb04e900e1e87bb9c069fd
Hp <Hp@DESKTOP-6KQ97H8>
2019-10-05 20:24:43 -0500

    agregamos getters y setters mas unos constructores

--- prac23/src/prac23/Libro.java
+++ prac23/src/prac23/Libro.java
@@ -6,5 +6,54 @@ package prac23;
  * @author 18PROGB0156
  */
 public class Libro {
+    private String Titulo; 
+    private String Autor;
+    private int existencia;
+    private int prestados;
+
+    public Libro() {
+    }
+    
+
+    public Libro(String Titulo, String Autor, int existencia, int prestados) {
+        this.Titulo = Titulo;
+        this.Autor = Autor;
+        this.existencia = existencia;
+        this.prestados = prestados;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+
+    public int getPrestados() {
+        return prestados;
+    }
+
+    public void setPrestados(int prestados) {
+        this.prestados = prestados;
+    }
+    
+    
     
 }
